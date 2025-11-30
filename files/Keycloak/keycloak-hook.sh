6e1014df11edfaf05093f66ad8c56c847b633a6f
Daniel-Doggy <gpg-signed@daniel-doggy.dev>
2025-11-30 11:18:41 +0100

    Fixed: path to ssl files

--- files/Keycloak/keycloak-hook.sh
+++ files/Keycloak/keycloak-hook.sh
@@ -2,9 +2,9 @@
 
 systemctl stop keycloak
 
-cp $RENEWED_LINEAGE/fullchain.pem /etc/keycloak/conf/server.crt.pem
-cp $RENEWED_LINEAGE/privkey.pem /etc/keycloak/conf/server.key.pem
-chown keycloak:keycloak /etc/keycloak/conf/server.crt.pem
-chown keycloak:keycloak /etc/keycloak/conf/server.key.pem
+cp $RENEWED_LINEAGE/fullchain.pem /opt/keycloak/conf/server.crt.pem
+cp $RENEWED_LINEAGE/privkey.pem /opt/keycloak/conf/server.key.pem
+chown keycloak:keycloak /opt/keycloak/conf/server.crt.pem
+chown keycloak:keycloak /opt/keycloak/conf/server.key.pem
 
 systemctl start keycloak
